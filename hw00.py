72736558cc1556c75ebe69ed177ae598b1523600
Lynn Wu <lwu61@smith.edu>
2026-09-16 13:33:38 -0400

    added another 3 print statements

--- hw00.py
+++ hw00.py
@@ -6,7 +6,9 @@
 # Define main function 
 def main():
 	print("Hello World!"); # print a message
-
+	print("printed line 2: (the default is to add a line jump at the end")
+	print("printed line 3: (this line will not add a default jump)", end="")
+	print("~~~ NOTE: this is NOT in a new line ~~~")
 
 # Run main if this script is being run 
 if __name__ == '__main__':
